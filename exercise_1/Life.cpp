c2d23df07188c327e5d7c067eb0ff858f6247c31
marcosicklinger <sicklinger.marco@gmail.com>
2023-05-21 17:46:58 +0400

    added: Life class

---
+++ exercise_1/Life.cpp
@@ -0,0 +1,5 @@
+//
+// Created by marcosicklinger on 5/21/23.
+//
+
+#include "Life.h"
---
+++ exercise_1/Life.h
@@ -0,0 +1,14 @@
+//
+// Created by marcosicklinger on 5/21/23.
+//
+
+#ifndef LIFE_H
+#define LIFE_H
+
+
+class Life {
+
+};
+
+
+#endif //LIFE_H
