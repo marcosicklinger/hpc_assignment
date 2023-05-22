f7618300120d4ab98958346daa332274e34e323f
marcosicklinger <sicklinger.marco@gmail.com>
2023-05-22 09:46:38 +0400

    added (Life): constructor, few Life methods declaration

--- exercise_1/Life.cpp
+++ exercise_1/Life.cpp
@@ -3,3 +3,14 @@
 //
 
 #include "Life.h"
+#include "utils.h"
+
+Life::Life(std::string &filename, int &_rows, int &_cols): rows(_rows), cols(_cols) {
+    state = read_state(filename);
+}
+
+void Life::transition(int &x, int &y) {}
+
+void Life::evolve() {}
+
+
--- exercise_1/Life.h
+++ exercise_1/Life.h
@@ -5,8 +5,22 @@
 #ifndef LIFE_H
 #define LIFE_H
 
+#include <string>
 
 class Life {
+
+public:
+
+    unsigned char *state = nullptr;
+    int rows, cols;
+
+    Life(std::string &filename, int &_rows, int &_cols);
+
+    void transition(int &x, int &y);
+
+    void evolve();
+
+private:
 
 };
 
