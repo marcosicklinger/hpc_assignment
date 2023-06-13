42b7cd9c4b4c524efb7e47baa81b5049ebec51cb
marcosicklinger <sicklinger.marco@gmail.com>
2023-06-13 22:03:56 +0400

    plotting story

--- exercise_1/src/app/story.py
+++ exercise_1/src/app/story.py
@@ -2,20 +2,23 @@ import os
 
 from life import *
 from PIL import Image
+from scipy import ndimage
 
 
-def create_gif_from_pgm_files(file_list, output_file):
+def save_story(file_list, output_file):
     history = []
+    frame_size = (500, 500)
     for file in file_list:
         image = load_pgm_snapshot(file)
-        pil_image = Image.fromarray(image.astype(np.uint8))
+        resized_image = ndimage.zoom(image, (frame_size[0] / image.shape[0], frame_size[1] / image.shape[1]))
+        pil_image = Image.fromarray(resized_image)
         history.append(pil_image)
 
-    history[0].save(output_file, save_all=True, append_images=history[1:], duration=400, loop=0)
+    history[0].save(output_file, save_all=True, append_images=history[1:], duration=600, loop=0)
 
 
 if __name__ == '__main__':
     path = '../../snapshot'
     file_list = list(map(lambda file: os.path.join(path, file), sorted(os.listdir(path))))
     output_file = '../../simulation/simulation.gif'
-    create_gif_from_pgm_files(file_list, output_file)
+    save_story(file_list, output_file)
