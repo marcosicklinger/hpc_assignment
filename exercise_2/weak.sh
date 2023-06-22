2e161a118b9ef899ce0b0c8cfaf4297e849a2290
marcosicklinger <sicklinger.marco@gmail.com>
2023-06-22 11:00:28 +0400

    removing: output commands from makefile (with @); adding: skeleton of weak run (in weak.sh)

--- exercise_2/weak.sh
+++ exercise_2/weak.sh
@@ -10,7 +10,29 @@
 #SBATCH --time=02:00:00
 #SBATCH --nodelist=epyc[005]
 
-module load architecture/AMD
-module load openBLAS/0.3.21-omp
-module load mkl
+#module load architecture/AMD
+#module load openBLAS/0.3.21-omp
+#module load mkl
 
+export OMP_NUM_THREADS=64
+export OMP_PROC_BIND=close
+export OMP_PLACES=cores
+
+minsize=2000
+maxsize=20000
+step=2000
+ntrials=10
+
+p="-DUSE_FLOAT"
+
+make clean
+make all PREC=$p
+
+#for((k=minsize; k<=maxsize; k+=step))
+#do
+#  for((i=0; i<ntrials; i++))
+#  do
+#    srun -n1 ./mkl.x "$k" "$k" $k >> t
+#    srun -n ./olas.x "$k" "$k" "$k" >> t
+#  done
+#done
