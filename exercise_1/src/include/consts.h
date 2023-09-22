24a097f3de935f90cd1aaebaffca239db773f14a
marcosicklinger <sicklinger.marco@gmail.com>
2023-09-22 13:53:25 +0200

    refactoring

--- exercise_1/src/include/consts.h
+++ exercise_1/src/include/consts.h
@@ -1,20 +1,26 @@
 #ifndef CONSTS_H
 #define CONSTS_H
 
-constexpr const char SNAPSHOT[] = "./snapshot/";
-constexpr const char TIME[] = "./time/";
-
+// prefix for snapshot filename
 constexpr const char FNAME_PREFIX[] = "./snapshot/snapshot_";
+
+// default name for first snapshot
 constexpr const char DFT_INIT_FNAME[] = "./snapshot/snapshot_00000";
 
+// cells' values
 constexpr int ALIVE = 0;
 constexpr int DEAD = 1;
 
+// default size
 constexpr unsigned int SIZE = 100;
 
+// default is ordered evolution
 constexpr int ORDERED = 0;
 
+// default number of time iterations
 constexpr unsigned int LIFETIME = 100;
+
+// default value for snapshot saving frequency
 constexpr unsigned int RECORD_EVERY = 20;
 
 #endif //CONSTS_H
